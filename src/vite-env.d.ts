5f8241c1f740f5d3c88ec3e27ae999c668739ce1
Aayan Bothra <110114427+aayanbothra@users.noreply.github.com>
2025-04-22 16:01:34 +0530

    Copy src/vite-env.d.ts from aayan-space-verse-folio

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+
+/// <reference types="vite/client" />
+
+interface Window {
+  gtag: (
+    command: 'config' | 'event' | 'js',
+    targetId: string,
+    config?: Record<string, any>
+  ) => void;
+  dataLayer: any[];
+}
